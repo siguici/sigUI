dee528026343fa1d2d3b7c13d23a1b5f8e053c4d
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-07-30 02:21:54 +0100

    :technologist: Add render method to the component contract

--- src/Contracts/Component.php
+++ src/Contracts/Component.php
@@ -4,4 +4,10 @@ namespace Sikessem\UI\Contracts;
 
 interface Component
 {
+    /**
+     * Get the view / view contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\Support\Htmlable|\Closure|string
+     */
+    public function render();
 }
