9821a87bcebbaa0bbe9245a87bdcbfd16feffa12
SIGUI Kessé Emmanuel <emmanuel@kesse.sigui.ci>
2023-08-07 05:28:04 +0100

    :triangular_flag_on_post: Check current input

--- res/views/components/input.blade.php
+++ res/views/components/input.blade.php
@@ -1,5 +1,5 @@
 @php
-$attributes = $attributes->merge([...compact('type', 'name', 'id', 'value'), 'invalid' => $invalid || $errors->has($name), 'aria-invalid' => $invalid || $errors->has($name) ? 'true' : 'false']);
+$attributes = $attributes->merge([...compact('type', 'name', 'id', 'value'), 'checked' => in_array($type, ['checkbox', 'radio']) && $chosen, 'invalid' => $invalid || $errors->has($name), 'aria-invalid' => $invalid || $errors->has($name) ? 'true' : 'false']);
 @endphp
 
 @tag('input', $attributes)
